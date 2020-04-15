cafe42b24ad4e0e65bc3e3cb8b954e7da2104d1e
Gorrie Coe <gorriecoe@gmail.com>
2020-04-16 07:40:23 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Silverstripe security links
+
+Adds a login, logout and lost password link types to [gorriecoe/silverstripe-link](https://github.com/gorriecoe/silverstripe-link)
+
+## Installation
+
+Composer is the recommended way of installing SilverStripe modules.
+
+```
+composer require gorriecoe/silverstripe-securitylinks
+```
+
+## Requirements
+
+- [gorriecoe/silverstripe-link](https://github.com/gorriecoe/silverstripe-link) ^1.2.3
+
+## Maintainers
+
+- [Gorrie Coe](https://github.com/gorriecoe)
---
+++ src/SecurityLinksExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace gorriecoe\SecurityLinks;
+
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Security\Security;
+
+/**
+ * Adds a login, logout and lost password link type to Link Object.
+ *
+ * @package silverstripe-securitylinks
+ */
+class SecurityLinksExtension extends DataExtension
+{
+    /**
+     * A map of object types that can be linked to
+     * Custom dataobjects can be added to this
+     *
+     * @var array
+     **/
+    private static $types = [
+        'Login' => 'Login link',
+        'Logout' => 'Logout link',
+        'LostPassword' => 'Lost password link',
+    ];
+
+    /**
+     * Update LinkURL
+     */
+    public function updateLinkURL(&$linkURL)
+    {
+        $owner = $this->owner;
+        switch ($owner->Type) {
+            case 'Login':
+                if (!Security::getCurrentUser()) {
+                    $linkURL = Security::login_url();
+                }
+                break;
+            case 'Logout':
+                if (Security::getCurrentUser()) {
+                    $linkURL = Security::logout_url();
+                }
+                break;
+            case 'LostPassword':
+                $linkURL = Security::lost_password_url();
+                break;
+        }
+    }
+}
